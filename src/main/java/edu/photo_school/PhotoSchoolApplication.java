9bedb18d7ad33566da53cb8efffbcec34b80ac20
melonegoist <408517@niuitmo.ru>
2025-12-11 12:14:25 +0300

    global: created spring core

---
+++ src/main/java/edu/photo_school/PhotoSchoolApplication.java
@@ -0,0 +1,13 @@
+package edu.photo_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoSchoolApplication.class, args);
+    }
+
+}
